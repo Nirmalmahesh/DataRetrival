5f7642022ab2c6cd532bff85e5a4510224fecf60
Nirmalmahesh S <nirmalmahesh54@gmail.com>
2019-08-09 12:02:55 +0530

    Basic Upto Login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {FormsModule} from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HeaderComponent } from './header/header.component';
+import { LoginComponent } from './login/login.component';
+import {HttpClientModule} from '@angular/common/http';
+import { UserDashboardComponent } from './user-dashboard/user-dashboard.component'
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LoginComponent,
+    UserDashboardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/auth.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+ url = "http://localhost:5000/"
+
+ navigate = () =>{
+   if(this.getUser())
+   {
+     this.router.navigate(['user']);
+   }
+ }
+ getUser = () =>{
+   let user = JSON.parse(sessionStorage.getItem("USER"));
+   if(user)
+   {
+    return user;
+   }else{
+     return null;
+   }
+ }
+  validateUser = (user) =>{
+    return this._http.post(this.url+'api/user/validateUser',user);
+  }
+  constructor(private _http : HttpClient,private router : Router) { }
+}
